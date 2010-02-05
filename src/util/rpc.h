2e96da5e21c10872449bb34df31dd23c755a6f9b
Russell Power <power@cs.nyu.edu>
2010-02-05 15:46:53 -0500

    Rename methods; cleanup logging.

--- src/util/rpc.h
+++ src/util/rpc.h
@@ -18,21 +18,21 @@ public:
 
   // Try to read a message from the given peer and rpc channel; return false if no
   // message is immediately available.
-  bool TryRead(int target, int rpc, Message *msg);
-  bool HasData(int target, int rpc);
-  bool HasData(int target, int rpc, MPI::Status &status);
+  bool TryRead(int target, int method, Message *msg);
+  bool HasData(int target, int method);
+  bool HasData(int target, int method, MPI::Status &status);
 
-  int Read(int target, int rpc, Message *msg);
-  int ReadAny(int *target, int rpc, Message *msg);
-  void Send(int target, int rpc, const Message &msg);
-  void SyncSend(int target, int rpc, const Message &msg);
+  int Read(int target, int method, Message *msg);
+  int ReadAny(int *target, int method, Message *msg);
+  void Send(int target, int method, const Message &msg);
+  void SyncSend(int target, int method, const Message &msg);
 
   MPI::Request SendData(int peer_id, int rpc_id, const string& data);
 
   // For whatever reason, MPI doesn't offer tagged broadcasts, we simulate that
   // here.
-  void Broadcast(int rpc, const Message &msg);
-  void SyncBroadcast(int rpc, const Message &msg);
+  void Broadcast(int method, const Message &msg);
+  void SyncBroadcast(int method, const Message &msg);
 
 private:
   boost::recursive_mutex mpi_lock_;
