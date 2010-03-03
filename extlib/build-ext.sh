377bdf5005b28b1bec0130145a116881d9475136
Russell Power <power@cs.nyu.edu>
2010-03-03 15:25:11 -0500

    Fix extlib build.

--- extlib/build-ext.sh
+++ extlib/build-ext.sh
@@ -5,12 +5,10 @@ CONFIG="./configure --enable-static --disable-dependency-tracking "
 (
 cd gflags
 $CONFIG && make clean && make libgflags.la -j8
-) &
+)
 
 (
 cd glog
 CPPFLAGS=-I../gflags/src/ LDFLAGS=-L../gflags/.libs $CONFIG && make clean && make libglog.la -j8
-) &
-
-wait
+)
 
