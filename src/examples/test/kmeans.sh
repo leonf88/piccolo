85652bfa7d5ddf294788c9f982b13b9135c48775
Russell Power <power@cs.nyu.edu>
2010-04-05 10:54:49 -0400

    Update testing scripts.

--- src/examples/test/kmeans.sh
+++ src/examples/test/kmeans.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-set -e
-
-rm -f kmeans.n*
-
-for n in 4 8 16 20; do 
-  for j in 0; do
-    echo $n $j
-    /usr/bin/time ~/share/bin/mpirun -hostfile mpi_hostfile -bynode  -tag-output -n $n \
-      bash -c 'LD_LIBRARY_PATH=/home/power/share/lib bin/example-dsm --runner=KMeans --iterations=50 --num_dists=10000 --num_points=500000 --sleep_time=0' &> kmeans.n_$n.r_$j
-  done
-done
+set -x
+source $(dirname $0)/run_util.sh
+run_command 'KMeans' '--iterations=10 --num_dists=100 --num_points=50000000 '
