b439ab4192a07da16e59fce867f71751ca249f4c
Russell Power <power@cs.nyu.edu>
2010-04-07 15:43:53 -0400

    Only use one core per worker.

--- src/examples/test/run_util.sh
+++ src/examples/test/run_util.sh
@@ -3,20 +3,31 @@
 set -e
 
 mkdir -p results/
+#strace -c -f -o results/trace.$n.\$BASHPID 
 
 function run_command() {
   runner=$1
   args=$2
-  for n in 1 2 4 8 16; do 
+  for n in 6 12 18 24; do 
       echo > results/$runner.n_$n
       echo "$runner :: $n"
       IFS=$(echo -e '\n')
       
-      /usr/bin/time ~/share/bin/mpirun -hostfile mpi_hostfile -bynode -tag-output -n $((n + 1)) \
-        bash -c "LD_LIBRARY_PATH=/home/power/share/lib bin/examples/example-dsm --runner=$runner $args --sleep_time=0.001"\
+      /usr/bin/time ~/share/bin/mpirun \
+         --mca mpi_paffinity_alone 1 \
+         -hostfile mpi_hostfile\
+         -bynode \
+         -tag-output -n $((n + 1)) \
+        bash -c "LD_LIBRARY_PATH=/home/power/share/lib \
+              bin/examples/example-dsm \
+        			--runner=$runner \
+        			$args "\
         2>&1 | while read line; do 
           echo $line >> results/$runner.n_$n
           echo $line
         done
+      if [[ $? -ne 0 ]]; then
+        exit 1
+      fi
   done
 }
