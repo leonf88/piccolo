607093d429edade2e1cf02891824ef93916d461d
Russell Power <power@cs.nyu.edu>
2010-04-26 14:08:36 -0400

    Get n-body working.

--- src/examples/test/nbody.sh
+++ src/examples/test/nbody.sh
@@ -6,13 +6,13 @@ source $(dirname $0)/run_util.sh
 CHECKPOINT_WRITE_DIR=/scratch/checkpoints/
 CHECKPOINT_READ_DIR=/scratch/cp-union/checkpoints/
 ITERATIONS=10
-PARALLELISM=2
-BUILD_TYPE=debug
-#BUILD_TYPE=release
+PARALLELISM=10
+#BUILD_TYPE=debug
+BUILD_TYPE=release
 
 function run_test() {
   run_command 'NBody' "\
- --particles=100 \
+ --particles=1000000 \
  --log_prefix=false \
  --sleep_time=0.001 \
  --iterations=$ITERATIONS \
