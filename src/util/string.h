39a07f24ef95f595ee222dc2a70ef716177d02df
Russell Power <power@cs.nyu.edu>
2010-05-19 11:10:16 -0400

    Cleaning up common code.

---
+++ src/util/string.h
@@ -0,0 +1,47 @@
+#ifndef UTIL_STRING_H_
+#define UTIL_STRING_H_
+
+#include <string>
+using std::string;
+
+class StringPiece {
+public:
+  StringPiece();
+  StringPiece(const string& s);
+  StringPiece(const string& s, int len);
+  StringPiece(const char* c);
+  StringPiece(const char* c, int len);
+  uint32_t hash() const;
+  string AsString() const;
+
+  const char* data;
+  int len;
+
+  static vector<StringPiece> split(StringPiece sp, StringPiece delim);
+};
+
+static bool operator==(const StringPiece& a, const StringPiece& b) {
+  return a.data == b.data && a.len == b.len;
+}
+
+static const char* strnstr(const char* haystack, const char* needle, int len) {
+  int nlen = strlen(needle);
+  for (int i = 0; i < len - nlen; ++i) {
+    if (strncmp(haystack + i, needle, nlen) == 0) {
+      return haystack + i;
+    }
+  }
+  return NULL;
+}
+
+#ifndef SWIG
+string StringPrintf(StringPiece fmt, ...);
+string VStringPrintf(StringPiece fmt, va_list args);
+#endif
+
+string ToString(int32_t);
+string ToString(int64_t);
+string ToString(string);
+string ToString(StringPiece);
+
+#endif /* STRING_H_ */
