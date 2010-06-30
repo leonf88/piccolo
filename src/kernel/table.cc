05ce3b3f9efebccab87ad73e47899e346a44eaa9
Russell Power <power@cs.nyu.edu>
2010-06-30 12:26:09 -0400

    Some more testing code. C++ templates and name collisions are not good for one's health.

--- src/kernel/table.cc
+++ src/kernel/table.cc
@@ -306,3 +306,46 @@ void LocalTable::ApplyUpdates(const HashPut& req) {
   }
 }
 }
+
+using namespace dsm;
+struct TableTestRGB { int r; int g; int b; };
+
+static void TestLocalTable() {
+  TableDescriptor td;
+  td.accum = new Accumulators<TableTestRGB>::Replace;
+  td.num_shards = 1;
+  td.shard = 0;
+  td.default_shard_size = 1;
+  td.table_id = 0;
+  td.key_marshal = td.value_marshal = td.sharder = NULL;
+  vector<tuple2<int, int> > source;
+  for (int i = 0; i < 50; ++i) {
+    for (int j = 0; j < 50; ++j) {
+      source.push_back(MP(i, j));
+    }
+  }
+
+  TableTestRGB b = { 1, 2, 3 };
+
+
+  TypedLocalTable<tuple2<int, int>, TableTestRGB> *h = new TypedLocalTable<tuple2<int, int>, TableTestRGB>;
+  h->Init(td);
+  h->resize(500000);
+
+  for (int j = 0; j < 2500; ++j) {
+    h->update(source[j], b);
+  }
+
+  Timer t;
+  for (int i = 0; i < 100; ++i) {
+    for (int j = 0; j < 2500; ++j) {
+      CHECK(h->contains(source[j]));
+      h->update(source[j], b);
+      CHECK(h->contains(source[j]));
+      h->get(source[j]);
+    }
+  }
+  LOG(INFO) << "Time: " << t.elapsed();
+}
+
+REGISTER_TEST(LocalTable, TestLocalTable());
