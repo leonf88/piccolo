cba2b34ceb05c1227ec727c90951d695f7266919
Russell Power <power@cs.nyu.edu>
2010-08-18 13:44:55 -0400

    Update crawler to reflect changes in table registry code.
    Add synchronous crawler mode to test for performance differences.

--- src/examples/test/crawler.py
+++ src/examples/test/crawler.py
@@ -3,16 +3,15 @@
 import sys; sys.path += ['src/examples/test']
 import runutil, math, os
 
-for n in [60]:#runutil.parallelism:
+for n in [2]:#runutil.parallelism:
   os.system('mkdir -p logs.%d' % n)
   os.system('rm logs.%d/*' % n)
   cmd = ' '.join(['mpirun',
-                  '-hostfile fast_hostfile',
                   '-bynode',
-                  '-nooversubscribe',
-                  '-display-map',
+ #                 '-hostfile conf/mpi-beakers',
+#                  '-nooversubscribe',
                   '-n %d' % (1 + n),
-                  'bin/release/examples/dsm-crawler',
+                  'python src/examples/crawler/crawler.py',
                   '--log_prefix=false',
                   '--crawler_runtime=300'])
   
