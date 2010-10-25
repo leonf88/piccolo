000610a8bf7bca95ce91a5a6fbcdc05c6e8e1211
Russell Power <power@cs.nyu.edu>
2010-10-25 13:30:42 -0400

    Add psuedo-macros documenting the preprocessor behavior for PMap, PRunOne and PRunAll.

--- src/client/client.h
+++ src/client/client.h
@@ -15,4 +15,18 @@ DECLARE_int32(shards);
 DECLARE_int32(iterations);
 #endif
 
+// These are expanded by the preprocessor; these macro definitions
+// are just for documentation.
+
+// Run the given block of code on a single shard of 'table'.
+#define PRunOne(table, code)
+
+// Run the given block of code once for all shards of 'table'.
+#define PRunAll(table, code)
+
+// The (value : table) entries in bindings are evaluated once for
+// each entry in table.  'code' is a code block that is
+// executed with the bindings provided, once for each table entry.
+#define PMap(bindings, code)
+
 #endif /* CLIENT_H_ */
